49a3b1392094a358da13a2c828a34106fa99a8ea
VexoriaAI <contato@vexoria.com.br>
2025-11-13 12:02:33 -0300

    Create materials.js

---
+++ database/materials.js
@@ -0,0 +1,39 @@
+/* ====================================================================
+// DATABASE: MATERIALS
+// Define todos os materiais brutos encontrados nos biomas.
+// ==================================================================== */
+
+const MATERIALS_DB = {
+    // --- Volcanics ---
+    'mat_metal': { name: "Metal", icon: "images/icons/materials/mat_metal.png" },
+    'mat_magma': { name: "Magma", icon: "images/icons/materials/mat_magma.png" },
+    'mat_volcanic_pumice_stone': { name: "Volcanic Pumice Stone", icon: "images/icons/materials/mat_volcanic_pumice_stone.png" },
+    'mat_obsidian_tears': { name: "Obsidian Tears", icon: "images/icons/materials/mat_obsidian_tears.png" },
+    
+    // --- Undergrounders ---
+    'mat_water': { name: "Water", icon: "images/icons/materials/mat_water.png" },
+    'mat_energized_crystals': { name: "Energized Crystals", icon: "images/icons/materials/mat_energized_crystals.png" },
+    'mat_thermal_water': { name: "Thermal Water", icon: "images/icons/materials/mat_thermal_water.png" },
+    'mat_special_clay': { name: "Special Clay", icon: "images/icons/materials/mat_special_clay.png" },
+    'mat_glass': { name: "Glass", icon: "images/icons/materials/mat_glass.png" },
+    
+    // --- Nocturnals ---
+    'mat_scrap': { name: "Scrap", icon: "images/icons/materials/mat_scrap.png" },
+    'mat_polymer': { name: "Polymer", icon: "images/icons/materials/mat_polymer.png" },
+    'mat_nanochips': { name: "Nanochips", icon: "images/icons/materials/mat_nanochips.png" },
+    'mat_cybernetic_implants': { name: "Cybernetic Implants", icon: "images/icons/materials/mat_cybernetic_implants.png" },
+    'mat_quantum_energy_core': { name: "Quantum Energy Core", icon: "images/icons/materials/mat_quantum_energy_core.png" },
+    
+    // --- Radioactives ---
+    'mat_strange_fluid': { name: "Strange Fluid", icon: "images/icons/materials/mat_strange_fluid.png" },
+    'mat_parasitic_fungus': { name: "Parasitic Fungus", icon: "images/icons/materials/mat_parasitic_fungus.png" },
+    'mat_venom_glands': { name: "Venom Glands", icon: "images/icons/materials/mat_venom_glands.png" },
+    'mat_luminescent_algae': { name: "Luminescent Algae", icon: "images/icons/materials/mat_luminescent_algae.png" },
+    
+    // --- Reptilians ---
+    'mat_food': { name: "Food", icon: "images/icons/materials/mat_food.png" },
+    'mat_healing_plants': { name: "Healing Plants", icon: "images/icons/materials/mat_healing_plants.png" },
+    'mat_hallucinogenic_fungi': { name: "Hallucinogenic Fungi", icon: "images/icons/materials/mat_hallucinogenic_fungi.png" },
+    'mat_animal_skin': { name: "Animal Skin", icon: "images/icons/materials/mat_animal_skin.png" },
+    'mat_reptilian_blood': { name: "Reptilian Blood", icon: "images/icons/materials/mat_reptilian_blood.png" }
+};
